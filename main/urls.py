a30a45ab9ade7f28828b44bd80d2f69228fd7363
dasifue <dasifue@gmail.com>
2024-12-09 11:40:20 +0600

    Added: users registration

--- main/urls.py
+++ main/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('app.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render 
+ 
+from .forms import UserCreationForm 
+ 
+def register(request): 
+    form = UserCreationForm() 
+ 
+    if request.method == "POST": 
+        form = UserCreationForm(data=request.POST) 
+        if form.is_valid(): 
+            form.save(commit=True) 
+ 
+    context = { 
+        'form': form 
+    } 
+    return render(request, 'register.html', context)
