146910edbb33805c08adf1b2258c20ee857b90ed
dasifue <dasifue@gmail.com>
2024-12-12 13:13:22 +0600

    Added: registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import UserRegistrationForm
+
+
+def register(request):
+
+    form = UserRegistrationForm()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+
+    context = {
+        "form": form
+    }
+    return render(request, "register.html", context)
