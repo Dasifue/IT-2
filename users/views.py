2daf6434a97a78fb72cde3d0de2bd455520f3512
dasifue <dasifue@gmail.com>
2024-12-13 13:55:36 +0600

    Added: login & logout

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 
 from .forms import UserRegistrationForm
 
@@ -16,3 +17,27 @@ def register(request):
         "form": form
     }
     return render(request, "register.html", context)
+
+
+def login_view(request):
+    message = ""
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            message = "Не удалось авторизоваться. Проверьте правильность данных"
+
+    return render(request, "login.html", {"message":message})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
