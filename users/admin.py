6faf816d9de0ea7e5808fa46dc355b4145ab4ed2
dasifue <dasifue@gmail.com>
2024-12-16 12:35:19 +0600

    Added: user's registartions

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
 
+from .forms import UserCreationForm
+
 from .models import User
 
 admin.site.unregister(Group)
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
     list_display = ['id', 'username', 'email', 'full_name', 'is_active']
     list_display_links = ['id', 'username', 'email', 'full_name']
     list_filter = ['is_staff', 'is_superuser', 'is_active']
@@ -31,6 +34,12 @@ class CustomUserAdmin(UserAdmin):
             {
                 "fields": ["is_staff", "is_superuser", "is_active"]
             }
+        ]
+    ]
+    add_fieldsets = [
+        [
+            None,
+            {"fields": ['username', 'email', 'full_name', 'password1', 'password2']}
         ]
     ]
     readonly_fields = ["date_joined", "last_login"]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .forms import UserCreationForm
+
+def register_view(request):
+
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+
+    return render(request, "register.html", {"form": form})
