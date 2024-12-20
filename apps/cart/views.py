b1f081089b0fe0caf00fcdfe741f22c26fb7b46b
dasifue <dasifue@gmail.com>
2024-12-20 11:07:13 +0600

    Added: add product to cart

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import Cart, CartProduct
+
+def add_to_cart_view(request, product_id):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    if request.user.cart.status == Cart.WAITING:
+        return redirect('index')
+
+    quantity = int(request.GET.get('quantity', 1))
+
+    cart_product = CartProduct.objects.filter(
+        cart=request.user.cart,
+        product_id=product_id
+    )
+    if cart_product.exists():
+        cart_product = cart_product.first()
+        cart_product.quantity += quantity
+        cart_product.save()
+    else:
+        CartProduct.objects.create(
+            cart=request.user.cart,
+            product_id=product_id,
+            quantity=quantity,
+        )
+
+    return redirect(request.META.get('HTTP_REFERER', '/'))
--- main/urls.py
+++ main/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('apps.app.urls')),
     path('users/', include('apps.users.urls')),
+    path('cart/', include('apps.cart.urls')),
     path('admin/', admin.site.urls),
 ]
 
