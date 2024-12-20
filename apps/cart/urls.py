e61fbaefb1951ee9161bf41329208112adb51c47
dasifue <dasifue@gmail.com>
2024-12-20 11:51:05 +0600

    Added: cart product views

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('add/<int:product_id>', views.add_to_cart_view, name='add_to_cart'),
     path('remove/<int:cart_product_id>', views.remove_from_cart_view, name='remove_from_cart'),
+    path('change-quantity/<int:cart_product_id>', views.change_cart_product_quantity_view, name='change_cart_product_quantity'),
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Cart, CartProduct
 
-def add_to_cart_view(request, product_id):
-    if not request.user.is_authenticated:
-        return redirect('login')
 
-    if request.user.cart.status == Cart.WAITING:
-        return redirect('index')
+def cart_active(func):
+    def wrapper(request, *args, **kwargs):
+        if request.user.cart.status == Cart.WAITING:
+            return redirect('index')
+        return func(request, *args, **kwargs)
+    return wrapper
+
+@login_required
+@cart_active
+def add_to_cart_view(request, product_id):
 
     quantity = int(request.GET.get('quantity', 1))
 
@@ -29,12 +35,9 @@ def add_to_cart_view(request, product_id):
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
+@login_required
+@cart_active
 def remove_from_cart_view(request, cart_product_id):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    
-    if request.user.cart.status == Cart.WAITING:
-        return redirect('index')
     
     try:
         cart_product = CartProduct.objects.get(id=cart_product_id)
@@ -42,5 +45,27 @@ def remove_from_cart_view(request, cart_product_id):
         pass
     else:
         cart_product.delete()
+    
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+@login_required
+@cart_active
+def change_cart_product_quantity_view(request, cart_product_id):
+    quantity = request.GET.get('quantity')
+    if quantity is None:
+        return redirect(request.META.get('HTTP_REFERER', '/'))
+    
+    quantity = int(quantity)
+    if 1 > quantity > 100:
+        return redirect(request.META.get('HTTP_REFERER', '/'))
+    
+    try:
+        cart_product = CartProduct.objects.get(id=cart_product_id)
+    except CartProduct.DoesNotExist:
+        return redirect(request.META.get('HTTP_REFERER', '/'))
+    else:
+        cart_product.quantity = quantity
+        cart_product.save()
     
     return redirect(request.META.get('HTTP_REFERER', '/'))
